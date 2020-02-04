a6f8879a8e9b333dadf8c051c06ade599a3f59f0
Pablo <pablofeo87@hotmail.com>
2020-02-04 17:45:36 -0300

    -

--- SpyStore.Dal/EfStructures/StoreContext.cs
+++ SpyStore.Dal/EfStructures/StoreContext.cs
@@ -9,18 +9,32 @@ namespace SpyStore.Dal.EfStructures
 {
     public class StoreContext : DbContext
     {
+        public int CustomerId { get; set; }
+
         public StoreContext(DbContextOptions<StoreContext> options) : base(options)
         {
 
         }
 
         public DbSet<Category> Categories { get; set; }
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<OrderDetail> OrderDetails { get; set; }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             modelBuilder.Entity<Customer>(entity =>
             {
                 entity.HasIndex(e => e.EmailAddress).HasName("IX_Customers").IsUnique();
+            });
+            modelBuilder.Entity<Order>(entity =>
+            {
+                entity.Property(e => e.OrderDate).HasColumnType("datetime").HasDefaultValueSql("getdate()");
+                entity.Property(e => e.ShipDate).HasColumnType("datetime").HasDefaultValueSql("getdate()");
+            });
+            modelBuilder.Entity<Order>().HasQueryFilter(x => x.CustomerId == CustomerId);
+            modelBuilder.Entity<OrderDetail>(entity =>
+            {
+                entity.Property(e => e.UnitCost).HasColumnType("money");
             });
         }
     }
